0901ecd4946847222bc002a7f201909b3414f3fd
Anirudh <arkbeasteditz@gmail.com>
2026-06-03 12:20:37 +0530

    fix: use neon pool for transactions

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,10 +1,16 @@
-import { neon } from '@neondatabase/serverless';
-import { drizzle } from 'drizzle-orm/neon-http';
+// import { neon } from '@neondatabase/serverless';
+// import { drizzle } from 'drizzle-orm/neon-http';
+// import * as schema from './schema';
+import { Pool } from '@neondatabase/serverless';
+import { drizzle } from 'drizzle-orm/neon-serverless';
 import * as schema from './schema';
 
+const pool = new Pool({ connectionString: process.env.DATABASE_URL! });
+export const db = drizzle(pool, { schema });
 if (!process.env.DATABASE_URL) {
   throw new Error('DATABASE_URL is not set in environment variables');
 }
 
-const sql = neon(process.env.DATABASE_URL);
-export const db = drizzle(sql, { schema });
+
+// const sql = neon(process.env.DATABASE_URL);
+// export const db = drizzle(sql, { schema });
